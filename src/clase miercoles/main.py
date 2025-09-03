81a33c39f79e6e790006e408745402fa1c7757b7
NicolasCaricati <nicolascaricati99@gmail.com>
2025-09-03 20:48:32 -0300

    upt

---
+++ src/clase miercoles/main.py
@@ -0,0 +1,13 @@
+# No hacer scripts sueltos de cada tema. Ir integrando todo en un solo script.
+# Dejar todo en un archivo bastante limpio y ordenado.
+# Hasta ahora agregar clase ternaria, arboles, bosques, obstuza
+
+def main():
+    print("Hola Mundo")
+
+if __name__ == "__main__":
+    main()
+    
+
+
+print("Hola Mundo")
