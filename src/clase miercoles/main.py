56c7615ecf8061238aa86121bb31ff1483db7ce6
NicolasCaricati <nicolascaricati99@gmail.com>
2025-09-25 18:59:25 -0300

    actualizacion

--- src/clase miercoles/main.py
+++ src/clase miercoles/main.py
@@ -45,7 +45,7 @@ def main():
     atributos = [c for c in df.columns if c not in columnas_a_excluir]
     columnas_Master = [c for c in df.columns if c.startswith("Master_")]
     columnas_Visa = [c for c in df.columns if c.startswith("Visa_")]      
-    columnas_categoricas = [c for c in df.columns if df[c].nunique() < 5]
+    columnas_categoricas = [c for c in df.colu.venv\Scripts\activatemns if df[c].nunique() < 5]
     columnas_a_excluir_ratios = ["foto_mes", "cliente_antiguedad","cliente_edad","numero_de_cliente","target",columnas_Master, columnas_Visa, columnas_categoricas] 
     # atributos_ratios = [c for c in df.columns if c not in columnas_a_excluir_ratios]
     atributos_ratios = ['mactivos_margen' , 'matm' , 'matm_other' , 'mautoservicio' , 'mcaja_ahorro' , 'mcaja_ahorro_adicional' , 'mcaja_ahorro_dolares' , 'mcajeros_propios_descuentos' , 'mcheques_depositados' , 'mcheques_depositados_rechazados' , 'mcheques_emitidos' , 'mcheques_emitidos_rechazados' , 'mcomisiones' , 'mcomisiones_mantenimiento' , 'mcomisiones_otras' , 'mcuenta_corriente' , 'mcuenta_corriente_adicional' , 'mcuenta_debitos_automaticos' , 'mcuentas_saldo' , 'mextraccion_autoservicio' , 'mforex_buy' , 'mforex_sell' , 'minversion1_dolares' , 'minversion1_pesos' , 'minversion2' , 'mpagodeservicios' , 'mpagomiscuentas' , 'mpasivos_margen' , 'mpayroll' , 'mpayroll2' , 'mplazo_fijo_dolares' , 'mplazo_fijo_pesos' , 'mprestamos_hipotecarios' , 'mprestamos_personales' , 'mprestamos_prendarios' , 'mrentabilidad' , 'mrentabilidad_annual' , 'mtarjeta_master_consumo' , 'mtarjeta_master_descuentos' , 'mtarjeta_visa_consumo' , 'mtarjeta_visa_descuentos' , 'mtransferencias_emitidas' , 'mtransferencias_recibidas' , 'mttarjeta_master_debitos_automaticos' , 'mttarjeta_visa_debitos_automaticos']
