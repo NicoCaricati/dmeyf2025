d19cc492e662c332f445c6d5f86d5afb605bb64d
NicolasCaricati <nicolascaricati99@gmail.com>
2025-10-28 21:08:45 -0300

    limpio main

--- src/competencia_02/main.py
+++ src/competencia_02/main.py
@@ -56,12 +56,6 @@ def main():
     """Pipeline principal con optimización usando configuración YAML."""
     logger.info("=== INICIANDO OPTIMIZACIÓN CON CONFIGURACIÓN YAML ===")
 
-    # if os.path.exists(os.path.join(BUCKET_NAME, "data", f"df_fe{STUDY_NAME}.csv")):
-    #     logger.info("✅ df_fe.csv encontrado")
-    #     df_fe = pd.read_csv(os.path.join(BUCKET_NAME, "data", f"df_fe{STUDY_NAME}.csv"))
-    # else:
-    #     logger.info("❌ df_fe.csv no encontrado")
-    
     path_parquet = os.path.join(BUCKET_NAME, "data", f"df_fe{STUDY_NAME}.parquet")
 
     if os.path.exists(path_parquet):
@@ -77,34 +71,6 @@ def main():
         logger.info(f"Datos cargados: {df.shape}")
     
     
-        # Saco cpayroll_trx por tener mucho drifting
-    
-        # # 1.5 PSI para detectar data drifting
-        # num_cols = df.select_dtypes(include=[np.number]).columns
-        # psi_resultados = psi_by_columns(df,num_cols, 202104, 202106,"foto_mes")
-        # psi_resultados.to_csv("feature_importance/psi_resultados.csv")
-        # Saco cpayroll_trx por tener mucho drifting
-        # df = df.drop(columns="cpayroll_trx")
-    
-    
-        # df_to_select_columns = pd.read_csv("feature_importance/feature_importance_sin_canarios.csv").sort_values("importance",ascending=False)
-    
-        
-        # Leer el archivo de importancias
-        # df_columnas_poco_importantes = pd.read_csv("feature_importance/feature_importance_Retesting...Saco Enero y Febrero para Limpiar FI 0 Vars_2025-10-10_12-56-35.csv")
-    
-        # # Filtrar las features con importance_split <= 1
-        # columnas_poco_importantes = df_columnas_poco_importantes.loc[
-        #     df_columnas_poco_importantes['importance_split'] == 0, 
-        #     'feature'
-        # ].tolist()
-    
-        # Top 40 de features de mayor importancia
-    
-        # # columnas_40_mas_importantes = df_to_select_columns.head(40)["feature"].to_list()
-        # columnas_40_mas_importantes = ["ctrx_quarter","mpayroll","cpayroll_trx","mprestamos_personales","mcuentas_saldo","mpasivos_margen","mcaja_ahorro","mtarjeta_visa_consumo","mrentabilidad_annual","Visa_msaldopesos","ctarjeta_visa_transacciones","cliente_edad","mactivos_margen","ctarjeta_master","Master_fechaalta","Visa_fechaalta","Visa_Fvencimiento","Visa_msaldototal","TC_Total_mpagospesos","TC_Total_mpagominimo","mtransferencias_recibidas","TC_Total_fechaalta","Master_Fvencimiento","numero_de_cliente","cliente_antiguedad","mrentabilidad","ctarjeta_debito_transacciones","TC_Total_msaldototal","chomebanking_transacciones","Visa_mpagospesos","ccomisiones_otras","Visa_mpagominimo","mcomisiones","mpayroll_corregida", "cpayroll_trx_corregida","ctrx_30d","ctrx_60d","saldo_total","uso_credito_ratio","TC_Total_msaldototal","uso_tarjeta_ratio","flujo_netotransf","uso_digital_ratio"]
-    
-    
         # 2. Feature Engineering
         # Excluyo las variables no corregidas
         cols_ajustar = [c for c in df.columns if c.startswith(('m', 'Visa_m', 'Master_m'))]
@@ -115,52 +81,18 @@ def main():
         columnas_base = df_fe.columns.tolist()
         columnas_a_excluir = ["foto_mes","cliente_edad","numero_de_cliente","target","target_to_calculate_gan"]
         atributos = [c for c in columnas_base if c not in columnas_a_excluir]
-        # df_fe = feature_engineering_ratio(df_fe,columnas_40_mas_importantes)
-    
-        # columnas_Master = [c for c in columnas_base if c.startswith("Master_")]
-        # columnas_Visa = [c for c in columnas_base if c.startswith("Visa_")]      
-        # columnas_categoricas = [c for c in columnas_base if df[c].nunique() < 5]
+
         for i in (1,2):
             df_fe = feature_engineering_lag(df_fe, columnas=atributos, cant_lag=i)
         for i in (1,2):
             df_fe = feature_engineering_delta(df_fe, columnas=atributos, cant_delta=i)
-        # df_fe = feature_engineering_delta(df_fe, columnas=atributos, cant_delta=2)
-        # df_fe = df_fe.astype({col: "float32" for col in df_fe.select_dtypes("float").columns})
         for i in (2,3):
             df_fe = feature_engineering_regr_slope_window(df_fe, columnas=atributos, ventana = i)
     
     
-    
-        # df_fe = feature_engineering_regr_slope_window(df_fe, columnas=atributos, ventana = 2)
-    
         variables_con_drfting =["Visa_Finiciomora","Master_fultimo_cierre","Visa_fultimo_cierre","Master_Finiciomora","cpayroll_trx","mpayroll"]
     
         df_fe = df_fe.drop(columns=variables_con_drfting, errors='ignore')
-        
-        # df_polars = pl.from_pandas(df_fe)  # si tu df original era Pandas
-
-        # excluir = ["numero_de_cliente", "target", "foto_mes", "target_to_calculate_gan"]
-        # columnas_a_normalizar = [c for c in df_polars.columns if c not in excluir and df_polars[c].dtype in [pl.Float64, pl.Float32, pl.Int64, pl.Int32]]
-
-        # df_polars = feature_engineering_robust_by_month_polars(df_polars, columnas=columnas_a_normalizar)
-        
-        # # Si querés volver a Pandas
-        # df_fe = df_polars.to_pandas()
-
-    
-        # df_fe = df_fe.astype({col: "float32" for col in df_fe.select_dtypes("float").columns})
-        # df_fe = df_fe[[c for c in df_fe.columns if not re.search(r'_delta_\d+_delta_', c)]]
-        # df_fe = df_fe[[c for c in df_fe.columns if not re.search(r'_delta_\d+_\d+$', c)]]
-        # df_fe = df_fe[[c for c in df_fe.columns if not re.search(r'lag\d+lag', c)]]
-        # df_fe = df_fe[[c for c in df_fe.columns if not re.search(r'lag\d+_\d+$', c)]]
-    
-        # df_fe = feature_engineering_variables_canarios(df_fe)
-    
-        # Eliminar las columnas poco importantes de df_fe
-        # df_fe = df_fe.drop(columns=columnas_poco_importantes, errors='ignore')
-    
-        # logger.info(f"Se eliminaron {len(columnas_poco_importantes)} columnas con importance_split <= 1")
-
         
         
         logger.info(f"Feature Engineering completado: {df_fe.shape}")
@@ -169,21 +101,14 @@ def main():
         # 3. Convertir clase_ternaria a binario
         df_fe = convertir_clase_ternaria_a_target(df_fe)
     
-        # df_fe = df_fe.astype({col: "float32" for col in df_fe.select_dtypes("float").columns})
-    
-        # df_fe.to_csv("data/competencia_fe_.csv", index=False)
-    
-    
-        # 4. Ejecutar optimización (función simple)
-
-        # df_fe.to_csv(os.path.join(BUCKET_NAME, "data", f"df_fe{STUDY_NAME}.csv"), index=False)
         df_fe.to_parquet(
             os.path.join(BUCKET_NAME, "data", f"df_fe{STUDY_NAME}.parquet"),
             compression='snappy'
         )
-
     
     logger.info("⏳ CSV cargado o creado, ahora ejecutando optimización...")
+
+    # 4. Ejecutar optimización (función simple)
     
     # study = optimizar(df_fe, n_trials=50,study_name = STUDY_NAME ,undersampling = 0.2)
   
@@ -224,18 +149,9 @@ def main():
 
     mejores_params = {'num_leaves': 169, 'learning_rate': 0.01653493811854045, 'min_data_in_leaf': 666, 'feature_fraction': 0.22865878320049338, 'bagging_fraction': 0.7317466615048293, 'num_boost_round': 682}
 
-    # mejores_params = {'num_leaves': 23, 'lr_init': 0.14053552566659705, 'min_data_in_leaf': 223, 'feature_fraction': 0.6616669584635271, 'bagging_fraction': 0.23994377622330532, 'num_boost_round': 439, 'lr_decay': 0.9124750514032693}
-
   
     logger.info("=== EVALUACIÓN EN CONJUNTO DE TEST ===")
     
-    # Evaluar en test
-    # resultados_test, y_pred_proba, y_test = evaluar_en_test_ensamble(df_fe, mejores_params)
-    
-    # res = comparar_semillas_en_grafico(df_fe, mejores_params, SEMILLA, study_name=STUDY_NAME)
-
-    # # Simular distribución de ganancias
-    # ganancias_sim = muestrear_ganancias(y_test, y_pred_proba)
 
     # Ejecutar comparación y graficar
     resultados_grafico = comparar_semillas_en_grafico_con_ensamble(
@@ -250,14 +166,6 @@ def main():
     logger.info("=== RESUMEN DE EVALUACIÓN EN TEST (ENSAMBLE TOTAL) ===")
     logger.info(f"✅ Ganancia en test: {res['ganancia_test']:,.0f}")
     logger.info(f"🎯 Predicciones positivas: {res['predicciones_positivas']:,} ({res['porcentaje_positivas']:.2f}%)")
-
-    
-     
-    # logger.info("=== GRAFICO DE TEST ===")
-
-    # # Grafico de test
-    # logger.info("=== GRAFICO DE TEST ===")
-    # ruta_grafico = crear_grafico_ganancia_avanzado(y_test,y_pred_proba)
 
 
     #06 Entrenar modelo final
@@ -292,3 +200,90 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+
+
+## Fin del código main.py
+
+## Codigo Muerto
+
+    
+        # Saco cpayroll_trx por tener mucho drifting
+    
+        # # 1.5 PSI para detectar data drifting
+        # num_cols = df.select_dtypes(include=[np.number]).columns
+        # psi_resultados = psi_by_columns(df,num_cols, 202104, 202106,"foto_mes")
+        # psi_resultados.to_csv("feature_importance/psi_resultados.csv")
+        # Saco cpayroll_trx por tener mucho drifting
+        # df = df.drop(columns="cpayroll_trx")
+    
+    
+        # df_to_select_columns = pd.read_csv("feature_importance/feature_importance_sin_canarios.csv").sort_values("importance",ascending=False)
+    
+        
+        # Leer el archivo de importancias
+        # df_columnas_poco_importantes = pd.read_csv("feature_importance/feature_importance_Retesting...Saco Enero y Febrero para Limpiar FI 0 Vars_2025-10-10_12-56-35.csv")
+    
+        # # Filtrar las features con importance_split <= 1
+        # columnas_poco_importantes = df_columnas_poco_importantes.loc[
+        #     df_columnas_poco_importantes['importance_split'] == 0, 
+        #     'feature'
+        # ].tolist()
+    
+        # Top 40 de features de mayor importancia
+    
+        # # columnas_40_mas_importantes = df_to_select_columns.head(40)["feature"].to_list()
+        # columnas_40_mas_importantes = ["ctrx_quarter","mpayroll","cpayroll_trx","mprestamos_personales","mcuentas_saldo","mpasivos_margen","mcaja_ahorro","mtarjeta_visa_consumo","mrentabilidad_annual","Visa_msaldopesos","ctarjeta_visa_transacciones","cliente_edad","mactivos_margen","ctarjeta_master","Master_fechaalta","Visa_fechaalta","Visa_Fvencimiento","Visa_msaldototal","TC_Total_mpagospesos","TC_Total_mpagominimo","mtransferencias_recibidas","TC_Total_fechaalta","Master_Fvencimiento","numero_de_cliente","cliente_antiguedad","mrentabilidad","ctarjeta_debito_transacciones","TC_Total_msaldototal","chomebanking_transacciones","Visa_mpagospesos","ccomisiones_otras","Visa_mpagominimo","mcomisiones","mpayroll_corregida", "cpayroll_trx_corregida","ctrx_30d","ctrx_60d","saldo_total","uso_credito_ratio","TC_Total_msaldototal","uso_tarjeta_ratio","flujo_netotransf","uso_digital_ratio"]
+    
+
+        # df_fe = feature_engineering_ratio(df_fe,columnas_40_mas_importantes)
+    
+        # columnas_Master = [c for c in columnas_base if c.startswith("Master_")]
+        # columnas_Visa = [c for c in columnas_base if c.startswith("Visa_")]      
+        # columnas_categoricas = [c for c in columnas_base if df[c].nunique() < 5]
+
+        # df_fe = df_fe.astype({col: "float32" for col in df_fe.select_dtypes("float").columns})
+
+
+                # df_polars = pl.from_pandas(df_fe)  # si tu df original era Pandas
+
+        # excluir = ["numero_de_cliente", "target", "foto_mes", "target_to_calculate_gan"]
+        # columnas_a_normalizar = [c for c in df_polars.columns if c not in excluir and df_polars[c].dtype in [pl.Float64, pl.Float32, pl.Int64, pl.Int32]]
+
+        # df_polars = feature_engineering_robust_by_month_polars(df_polars, columnas=columnas_a_normalizar)
+        
+        # # Si querés volver a Pandas
+        # df_fe = df_polars.to_pandas()
+
+    
+        # df_fe = df_fe.astype({col: "float32" for col in df_fe.select_dtypes("float").columns})
+        # df_fe = df_fe[[c for c in df_fe.columns if not re.search(r'_delta_\d+_delta_', c)]]
+        # df_fe = df_fe[[c for c in df_fe.columns if not re.search(r'_delta_\d+_\d+$', c)]]
+        # df_fe = df_fe[[c for c in df_fe.columns if not re.search(r'lag\d+lag', c)]]
+        # df_fe = df_fe[[c for c in df_fe.columns if not re.search(r'lag\d+_\d+$', c)]]
+    
+        # df_fe = feature_engineering_variables_canarios(df_fe)
+    
+        # Eliminar las columnas poco importantes de df_fe
+        # df_fe = df_fe.drop(columns=columnas_poco_importantes, errors='ignore')
+    
+        # logger.info(f"Se eliminaron {len(columnas_poco_importantes)} columnas con importance_split <= 1")
+
+
+
+    # Evaluar en test
+    # resultados_test, y_pred_proba, y_test = evaluar_en_test_ensamble(df_fe, mejores_params)
+    
+    # res = comparar_semillas_en_grafico(df_fe, mejores_params, SEMILLA, study_name=STUDY_NAME)
+
+    # # Simular distribución de ganancias
+    # ganancias_sim = muestrear_ganancias(y_test, y_pred_proba)
+             
+    # logger.info("=== GRAFICO DE TEST ===")
+
+    # # Grafico de test
+    # logger.info("=== GRAFICO DE TEST ===")
+    # ruta_grafico = crear_grafico_ganancia_avanzado(y_test,y_pred_proba)
+
+
+        # mejores_params = {'num_leaves': 23, 'lr_init': 0.14053552566659705, 'min_data_in_leaf': 223, 'feature_fraction': 0.6616669584635271, 'bagging_fraction': 0.23994377622330532, 'num_boost_round': 439, 'lr_decay': 0.9124750514032693}
